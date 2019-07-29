cbe2555e6edca2f2b94ca070e52d45ac8b1a5487
Georgi Popov <popovga@gmail.com>
2019-07-29 09:37:34 +0300

    Rearrange folder structure. Every module is now in a separate folder, and tests are a subfolder of the module

---
+++ engine/test/src/test_dummy.cpp
@@ -0,0 +1,6 @@
+#include <catch2/catch.hpp>
+
+TEST_CASE("Always Succeeds", "[dummy]")
+{
+    REQUIRE(1 == 1);
+}
---
+++ visualizer/test/src/test_dummy.cpp
@@ -0,0 +1,5 @@
+#include <catch2/catch.hpp>
+TEST_CASE("Always Succeeds", "[dummy]")
+{
+    REQUIRE(1 == 1);
+}
