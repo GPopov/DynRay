c5c30aacbee226a36d397fd5b10c86d05c3cfb1c
Georgi Popov <popovga@gmail.com>
2019-08-01 00:15:46 +0300

    Create simple unique_ptr wrappers around some SDL primitives (Texture, Window, Renderer)

---
+++ visualizer/include/SDLWrappers.h
@@ -0,0 +1,26 @@
+#include <memory>
+#include <SDL.h>
+
+#define DYNRAY_SDL_CUSTOM_DELETER(WrappedClass)      \
+    struct SDL_##WrappedClass##Deleter                  \
+    {                                                \
+        void operator()(SDL_##WrappedClass *wrapped) \
+        {                                            \
+            SDL_Destroy##WrappedClass(wrapped);      \
+        }                                            \
+    };
+
+namespace DynRay
+{
+    namespace Visualizer
+    {
+        DYNRAY_SDL_CUSTOM_DELETER(Renderer);
+        DYNRAY_SDL_CUSTOM_DELETER(Texture);
+        DYNRAY_SDL_CUSTOM_DELETER(Window);
+
+        using SDL_RendererPtr = std::unique_ptr<SDL_Renderer, SDL_RendererDeleter>;
+        using SDL_TexturePtr = std::unique_ptr<SDL_Texture, SDL_TextureDeleter>;
+        using SDL_WindowPtr = std::unique_ptr<SDL_Window, SDL_WindowDeleter>;
+    }
+}
+
