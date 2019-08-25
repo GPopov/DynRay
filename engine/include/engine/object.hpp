05f0e4e607ffb9669f674d312734f3bb241de0d8
DIVINASDESKTOP\georgi.popov <popovga@gmail.com>
2019-08-25 18:53:38 +0300

    Add support for planes

--- engine/include/engine/object.hpp
+++ engine/include/engine/object.hpp
@@ -8,24 +8,35 @@ namespace Engine
     struct Object
     {
         virtual ~Object() = default;
-        virtual glm::vec4 GetNormal(const glm::vec4& point) const = 0;
-        virtual void GetSurfaceDataAt(const glm::vec4 &point, glm::vec4 &outNormal, glm::vec2 &outTexCoords) = 0;
-        virtual glm::vec4 GetColorAt(const glm::vec2 &texCoords) const = 0;
-        virtual float Intersect(const glm::vec4 &rayOrigin, const glm::vec4 &rayDirection) const = 0;
+        virtual void GetSurfaceDataAt(const glm::vec4& point, glm::vec4& outNormal, glm::vec2& outTexCoords) = 0;
+        virtual glm::vec4 GetColorAt(const glm::vec2& texCoords, const glm::vec4& hitNormal) const = 0;
+        virtual float Intersect(const glm::vec4& rayOrigin, const glm::vec4& rayDirection) const = 0;
     };
 
     struct Sphere : public Object
     {
-        glm::vec4 GetNormal(const glm::vec4& point) const override;
-        void GetSurfaceDataAt(const glm::vec4 &point, glm::vec4 &outNormal, glm::vec2 &outTexCoords) override;
+        glm::vec4 GetNormal(const glm::vec4& point) const;
+        void GetSurfaceDataAt(const glm::vec4& point, glm::vec4& outNormal, glm::vec2& outTexCoords) override;
 
-        glm::vec4 GetColorAt(const glm::vec2& texCoords) const override;
-        float Intersect(const glm::vec4 &rayOrigin, const glm::vec4 &rayDirection) const override;
+        glm::vec4 GetColorAt(const glm::vec2& texCoords, const glm::vec4& hitNormal) const override;
+        float Intersect(const glm::vec4& rayOrigin, const glm::vec4& rayDirection) const override;
 
         glm::vec4 m_Color;
         glm::vec4 m_Center;
         float m_Radius;
     };
+
+	struct Plane : public Object
+	{
+		void GetSurfaceDataAt(const glm::vec4& point, glm::vec4& outNormal, glm::vec2& outTexCoords) override;
+
+		glm::vec4 GetColorAt(const glm::vec2& texCoords, const glm::vec4& hitNormal) const override;
+		float Intersect(const glm::vec4& rayOrigin, const glm::vec4& rayDirection) const override;
+
+		glm::vec4 m_Normal;
+		glm::vec4 m_Pos;
+		glm::vec4 m_Color;
+	};
 
 } // namespace Engine
 } // namespace DynRay
--- engine/src/object.cpp
+++ engine/src/object.cpp
@@ -10,14 +10,14 @@ namespace Engine
     {
         return (point - m_Center) * (-1 / m_Radius);
     }
-    glm::vec4 Sphere::GetColorAt(const glm::vec2& texCoords) const
+    glm::vec4 Sphere::GetColorAt(const glm::vec2& texCoords, const glm::vec4& hitNormal) const
     {
         constexpr float scale = 4.f;
         float pattern = (fmodf(texCoords.x * scale, 1.f) > 0.5f) ^ (fmodf(texCoords.y * scale, 1.f) > 0.5f);
 
         return glm::mix(m_Color, m_Color * 0.8f, pattern);
     }
-    void Sphere::GetSurfaceDataAt(const glm::vec4 &point, glm::vec4 &outNormal, glm::vec2 &outTexCoords)
+    void Sphere::GetSurfaceDataAt(const glm::vec4& point, glm::vec4& outNormal, glm::vec2& outTexCoords)
     {
         outNormal = GetNormal(point);
         
@@ -25,7 +25,7 @@ namespace Engine
         outTexCoords.y = glm::acos(outNormal.y) * glm::one_over_pi<float>();
     }
 
-    float Sphere::Intersect(const glm::vec4 &rayOrigin, const glm::vec4 &rayDirection) const
+    float Sphere::Intersect(const glm::vec4& rayOrigin, const glm::vec4& rayDirection) const
     {
         float distance = -1.f;
         bool result = glm::intersectRaySphere(rayOrigin, rayDirection, m_Center, m_Radius * m_Radius, distance);
@@ -34,5 +34,28 @@ namespace Engine
 
         return distance;
     }
+
+	void Plane::GetSurfaceDataAt(const glm::vec4& point, glm::vec4& outNormal, glm::vec2& outTexCoords)
+	{
+		outNormal = m_Normal;
+	}
+
+	glm::vec4 Plane::GetColorAt(const glm::vec2& texCoords, const glm::vec4& hitNormal) const
+	{
+		return m_Color;
+	}
+
+	float Plane::Intersect(const glm::vec4& rayOrigin, const glm::vec4& rayDirection) const
+	{
+		float denom = glm::dot(m_Normal, rayDirection);
+		if (glm::abs(denom) > glm::epsilon<float>()) 
+		{
+			glm::vec4 p0l0 = m_Pos - rayOrigin;
+			return glm::dot(p0l0, m_Normal) / denom;
+		}
+
+		return -1.f;
+	}
+
 }
 }
