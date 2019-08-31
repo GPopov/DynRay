9592739bbf6f6b341b2a43b3f7e2a78dbdc1a949
DIVINASDESKTOP\georgi.popov <popovga@gmail.com>
2019-08-31 16:25:39 +0300

    Add tests for plane normals

--- engine/test/src/test_normals.cpp
+++ engine/test/src/test_normals.cpp
@@ -25,3 +25,21 @@ TEST_CASE("Sphere object normals", "[normals]")
     CHECK(normal.w == Approx(expectedNormal.w));
     CHECK(glm::length(expectedNormal) == Approx(1.f));
 }
+
+TEST_CASE("Plane object normals", "[normals]")
+{
+    
+    auto plane = std::make_unique<DynRay::Engine::Plane>(glm::normalize(glm::vec4(0.f, 0.f, 1.f, 0.f)), glm::vec4(0.f, 0.f, 0.f, 1.f));
+    plane->m_Name = "P1";
+
+    glm::vec4 normal; glm::vec2 uv;
+    plane->GetSurfaceDataAt(glm::vec4(-1.f, 3.f, 0.f, 1.f), normal, uv);
+
+    glm::vec4 expectedNormal = glm::vec4(0.f, 0.f, 1.f, 0.f);
+
+    CHECK(normal.x == Approx(expectedNormal.x));
+    CHECK(normal.y == Approx(expectedNormal.y));
+    CHECK(normal.z == Approx(expectedNormal.z));
+    CHECK(normal.w == Approx(expectedNormal.w));
+    CHECK(glm::length(expectedNormal) == Approx(1.f));
+}
