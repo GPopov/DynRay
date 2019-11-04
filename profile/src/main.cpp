aed282d7c57e2349421387a762e8ddefe9760f2c
DIVINASDESKTOP\georgi.popov <popovga@gmail.com>
2019-11-04 23:05:34 +0200

    Profile individual Scene Traces, for real this time

--- profile/src/main.cpp
+++ profile/src/main.cpp
@@ -1,6 +1,7 @@
 #include <benchmark/benchmark.h>
 #include <random>
 #include <array>
+#include <iostream>
 #include "engine/scene.hpp"
 #include "engine/renderer.hpp"
 #include "engine/camera.hpp"
@@ -31,13 +32,13 @@ DynRay::Engine::Scene PrepareScene()
        auto sphere = DynRay::Engine::Sphere();
        sphere.m_Center = glm::vec4(disPos(gen), disPos(gen), disDistance(gen), 1.f);
        sphere.m_Radius = disSize(gen);
-       sphere.m_Material = DynRay::Engine::DiffuseMaterial(glm::vec4(disColor(gen), disColor(gen), disColor(gen), 1.f));
+       //sphere.m_Material = DynRay::Engine::DiffuseMaterial(glm::vec4(disColor(gen), disColor(gen), disColor(gen), 1.f));
        scene.AddObject(std::move(sphere));
        ++generatedSpheres;
    }
    {
        auto plane = DynRay::Engine::Plane(glm::normalize(glm::vec4(0.f, 1.f, 0.f, 0.f)), glm::vec4(0.f, -3.f, -20.f, 1.f));
-       plane.m_Material = DynRay::Engine::DiffuseMaterial(glm::vec4(0.f, 0.f, 1.f, 1.f));
+       //plane.m_Material = DynRay::Engine::DiffuseMaterial(glm::vec4(0.f, 0.f, 1.f, 1.f));
        scene.AddObject(std::move(plane));
    }
 
@@ -52,7 +53,7 @@ DynRay::Engine::Camera PrepareCamera()
 }
 }
 
-static void BM_Render(benchmark::State &state)
+static void BM_Trace(benchmark::State &state)
 {
    auto scene = PrepareScene();
    auto camera = PrepareCamera();
@@ -64,13 +65,16 @@ static void BM_Render(benchmark::State &state)
    std::mt19937 gen(rd());
    std::uniform_int_distribution<uint32_t> disX(0, WIDTH - 1);
    std::uniform_int_distribution<uint32_t> disY(0, HEIGHT - 1);
+   
+
    // Perform setup here
    for (auto _ : state)
    {
-       DynRay::Engine::Renderer::RenderSinglePixel(scene, camera, WIDTH, HEIGHT, buffer->data(), disX(gen), disY(gen));
+       glm::vec4 rayDirection = camera.GeneratePrimaryRayDirection(WIDTH, HEIGHT, disX(gen), disY(gen));
+       scene.Trace(camera.GetPosition(), rayDirection, 0.f);
    }
 }
  //Register the function as a benchmark
-BENCHMARK(BM_Render);
+BENCHMARK(BM_Trace);
 
 BENCHMARK_MAIN();
