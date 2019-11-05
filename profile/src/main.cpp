332ca669a848e6b145e91e1f38bbff03ee5426c9
DIVINASDESKTOP\georgi.popov <popovga@gmail.com>
2019-11-05 22:05:19 +0200

    Create a few benchmarks that measure individually Tracing a single ray, rendering a single pixel & rendering the whole scene

--- profile/src/main.cpp
+++ profile/src/main.cpp
@@ -32,13 +32,13 @@ DynRay::Engine::Scene PrepareScene()
        auto sphere = DynRay::Engine::Sphere();
        sphere.m_Center = glm::vec4(disPos(gen), disPos(gen), disDistance(gen), 1.f);
        sphere.m_Radius = disSize(gen);
-       //sphere.m_Material = DynRay::Engine::DiffuseMaterial(glm::vec4(disColor(gen), disColor(gen), disColor(gen), 1.f));
+       sphere.m_Material = DynRay::Engine::DiffuseMaterial(glm::vec4(disColor(gen), disColor(gen), disColor(gen), 1.f));
        scene.AddObject(std::move(sphere));
        ++generatedSpheres;
    }
    {
        auto plane = DynRay::Engine::Plane(glm::normalize(glm::vec4(0.f, 1.f, 0.f, 0.f)), glm::vec4(0.f, -3.f, -20.f, 1.f));
-       //plane.m_Material = DynRay::Engine::DiffuseMaterial(glm::vec4(0.f, 0.f, 1.f, 1.f));
+       plane.m_Material = DynRay::Engine::DiffuseMaterial(glm::vec4(0.f, 0.f, 1.f, 1.f));
        scene.AddObject(std::move(plane));
    }
 
@@ -51,30 +51,56 @@ DynRay::Engine::Camera PrepareCamera()
    camera.m_VerticalFOV = glm::radians(45.f);
    return camera;
 }
-}
 
-static void BM_Trace(benchmark::State &state)
+class Fixture : public benchmark::Fixture
 {
-   auto scene = PrepareScene();
-   auto camera = PrepareCamera();
-   constexpr uint32_t WIDTH = 1024;
-   constexpr uint32_t HEIGHT = 768;
-   auto buffer = std::make_unique<std::array<uint32_t, WIDTH * HEIGHT>>();
+public:
+    static constexpr uint32_t WIDTH = 1024;
+    static constexpr uint32_t HEIGHT = 768;
+    DynRay::Engine::Scene m_Scene;
+    DynRay::Engine::Camera m_Camera;
+    std::array < uint32_t, WIDTH * HEIGHT> m_Buffer;
+    std::mt19937 m_rng;
+    std::uniform_int_distribution<uint32_t> m_DisX{ 0, WIDTH - 1 };
+    std::uniform_int_distribution<uint32_t> m_DisY{ 0, HEIGHT - 1 };
 
-   std::random_device rd;
-   std::mt19937 gen(rd());
-   std::uniform_int_distribution<uint32_t> disX(0, WIDTH - 1);
-   std::uniform_int_distribution<uint32_t> disY(0, HEIGHT - 1);
-   
-
-   // Perform setup here
-   for (auto _ : state)
-   {
-       glm::vec4 rayDirection = camera.GeneratePrimaryRayDirection(WIDTH, HEIGHT, disX(gen), disY(gen));
-       scene.Trace(camera.GetPosition(), rayDirection, 0.f);
-   }
+    void SetUp(const ::benchmark::State& state) override {
+        m_Scene = PrepareScene();
+        m_Camera = PrepareCamera();
+        std::random_device rd;
+        m_rng.seed(rd());
+    }
+};
 }
- //Register the function as a benchmark
-BENCHMARK(BM_Trace);
+
+BENCHMARK_DEFINE_F(Fixture, RenderWholeScene_NoHeapAlloc)(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        DynRay::Engine::Renderer::Render(m_Scene, m_Camera, WIDTH, HEIGHT, m_Buffer.data());
+    }
+}
+
+BENCHMARK_DEFINE_F(Fixture, TraceSingleRay_NoHeapAlloc)(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        glm::vec4 rayDir = m_Camera.GeneratePrimaryRayDirection(WIDTH, HEIGHT, m_DisX(m_rng), m_DisY(m_rng));
+        m_Scene.Trace(m_Camera.GetPosition(), rayDir, 0.f);
+    }
+}
+
+BENCHMARK_DEFINE_F(Fixture, RenderSinglePixel_NoHeapAlloc)(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        DynRay::Engine::Renderer::RenderSinglePixel(m_Scene, m_Camera, WIDTH, HEIGHT, m_Buffer.data(), m_DisX(m_rng), m_DisY(m_rng));
+    }
+}
+
+BENCHMARK_REGISTER_F(Fixture, RenderWholeScene_NoHeapAlloc)->Unit(benchmark::kMillisecond);
+BENCHMARK_REGISTER_F(Fixture, TraceSingleRay_NoHeapAlloc);
+BENCHMARK_REGISTER_F(Fixture, RenderSinglePixel_NoHeapAlloc);
+
 
 BENCHMARK_MAIN();
