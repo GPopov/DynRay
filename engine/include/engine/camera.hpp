917f37c82742b917567561102829411abdda95c1
DIVINASDESKTOP\georgi.popov <popovga@gmail.com>
2019-12-28 02:02:23 +0200

    * Fix camera inconsitency (was using ViewMatrix where World Matrix should've been used). * Make camera FoV be true FoV (was previously half-angle) * Fix unit tests to accomodate the prior changes * Enable collision unit tests (were disabled and unfixed since merge to non-heap allocated management of objects)

--- engine/include/engine/camera.hpp
+++ engine/include/engine/camera.hpp
@@ -11,20 +11,20 @@ namespace Engine
       glm::vec4 RasterToCameraSpace(const glm::vec3& rasterPoint, const RenderOptions& renderOptions) const;
       glm::vec4 RasterToWorldSpace(const glm::vec3& rasterPoint, const RenderOptions& renderOptions) const;
       inline glm::vec4 GeneratePrimaryRayDirection(const RenderOptions& renderOptions, uint32_t x, uint32_t y) const;
-      const glm::vec4& GetPosition() const {return m_ViewMatrix[3]; };
+      const glm::vec4& GetPosition() const {return m_ToWorldMatrix[3]; };
       void SetCameraMatrix(const glm::mat4x4& viewMatrix );
 
         glm::mat4x4 m_ViewMatrix = glm::mat4x4(1.f);
         glm::mat4x4 m_ToWorldMatrix = glm::inverse(glm::mat4x4(1.f));
-        float m_VerticalFOV = 1.0472f; //60 deg in rad default
+        float m_VerticalFOV = glm::radians(90.f);
     };
 
 	inline glm::vec4 Camera::GeneratePrimaryRayDirection(const RenderOptions& renderOptions, uint32_t x, uint32_t y) const
 	{
-		glm::vec4 cameraSpaceCoords = RasterToCameraSpace(glm::vec3(x, y, -1.f), renderOptions);
-		cameraSpaceCoords.w = 0.f;
-		glm::vec4 rayDirection = m_ViewMatrix * cameraSpaceCoords;
-		rayDirection = glm::normalize(rayDirection);
+        glm::vec4 cameraSpaceCoords = RasterToCameraSpace({ x,y,-1.f }, renderOptions);
+        cameraSpaceCoords.w = 0.f;
+        auto rayDirection = m_ToWorldMatrix * cameraSpaceCoords;
+        rayDirection = glm::normalize(rayDirection);;
 		return rayDirection;
 	}
 }
